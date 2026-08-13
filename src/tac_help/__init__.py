6c3a1d5d3d7bf3b717bba73f73ff39b5ad6edee0
Арсений <theazdacompany@atomicmail.io>
2026-08-13 15:31:06 +0300

    Add files via upload

--- src/tac_help/__init__.py
+++ src/tac_help/__init__.py
@@ -8,7 +8,7 @@ from .vip import ask_ai, summarize, fetch_json
 from .utils import load_json, save_json, log_info, log_warn, log_error
 from .cli import main as cli_main
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "color", "pause", "clear",
     "activate", "get_tier", "has_access", "set_license_key",
