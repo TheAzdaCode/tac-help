cb8992fe4bcf9ec67571b65c000736f3d2001b9f
Арсений <theazdacompany@atomicmail.io>
2026-08-13 12:47:13 +0300

    Create __init__.py

---
+++ src/tac_help/__init__.py
@@ -0,0 +1,20 @@
+# tac_help/__init__.py
+
+from .core import color, pause, clear
+from .license import activate, get_tier, has_access, set_license_key
+from .pro import progress_bar, timer, retry
+from .premium import print_table, input_int, input_choice, menu
+from .vip import ask_ai, summarize, fetch_json
+from .utils import load_json, save_json, log_info, log_warn, log_error
+from .cli import main as cli_main
+
+__version__ = "0.2.0"
+__all__ = [
+    "color", "pause", "clear",
+    "activate", "get_tier", "has_access", "set_license_key",
+    "progress_bar", "timer", "retry",
+    "print_table", "input_int", "input_choice", "menu",
+    "ask_ai", "summarize", "fetch_json",
+    "load_json", "save_json", "log_info", "log_warn", "log_error",
+    "cli_main",
+]
